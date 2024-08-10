ea01d7a8a06a75e6e29e732f21245a6bad0d85d1
ramy-arab <160538077+ramy-arab@users.noreply.github.com>
2024-08-10 05:56:37 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,10 @@ import cors from 'cors';
 
 const app = express();
 
-// Middleware for parsing request body
 app.use(express.json());
 
-// Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
+
 app.use(cors());
-// Option 2: Allow Custom Origins                                  (taha akhir )
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
 
 
 dotenv.config();
